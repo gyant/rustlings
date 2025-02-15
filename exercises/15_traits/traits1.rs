643f3e8a334da05fd57b9ad34e43f128e8aa01e3
Ryan Thompson <ryan@zengyant.com>
2025-02-14 22:03:40 -0700

    progress through errors / generics/ traits

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -6,6 +6,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        let bar = String::from("Bar");
+        self + &bar
+    }
 }
 
 fn main() {
